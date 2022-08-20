bd8420f802acf29d5c7082e6bc4006a40993fdcb
Nguyen Tri Phuc <109999741+phucnthe176442@users.noreply.github.com>
2022-08-20 22:56:01 +0700

    new restrict

--- src/main/util/RestrictedUtils.java
+++ src/main/util/RestrictedUtils.java
@@ -1,152 +1,67 @@
-package main;
+package main.util;
 
-import io.socket.emitter.Emitter.Listener;
-import jsclub.codefest.sdk.algorithm.AStarSearch;
-import jsclub.codefest.sdk.socket.data.*;
-import jsclub.codefest.sdk.util.GameUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import jsclub.codefest.sdk.model.Hero;
+import jsclub.codefest.sdk.socket.data.Human;
+import jsclub.codefest.sdk.socket.data.MapInfo;
+import jsclub.codefest.sdk.socket.data.Position;
+import jsclub.codefest.sdk.socket.data.Viruses;
 
-import main.constant.GameConfig;
-import main.util.RestrictedUtils;
+public class RestrictedUtils {
+    public static Set<Position> PERMANENT_RESTRICTED_NODE_SET = ConcurrentHashMap.newKeySet();
+    public static final boolean BOMB_INCLUDED = true;
+    public static final boolean BOMB_NOT_INCLUDED = false;
 
-import java.util.*;
+    public static List<Position> getRestrictedNode(MapInfo mapInfo, Hero hero, Boolean bombIncluded) {
+        Set<Position> restrictedNode = ConcurrentHashMap.newKeySet();
 
-import static java.lang.Math.*;
+        restrictedNode.addAll(getPermanentRestrictedNode(mapInfo));
+        restrictedNode.addAll(mapInfo.balk);
 
-public class RandomPlayer {
-    final static String SERVER_URL = "https://codefest.jsclub.me/";
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, -1, 0, 1};
-    public static int[][] visit, matrix;
-    public static List<Position> valid_cells = new ArrayList<>();
-    public static List<Position> restrictNode = new ArrayList<>();
-    public static int n, m;
-    public static void main(String[] args) {
-        // Creating a new Hero object with name `player1-xxx` and game id
-        // `GameConfig.GAME_ID`.
-        Hero player1 = new Hero("player1-xxx", GameConfig.GAME_ID);
-        TimestampManager tm = new TimestampManager();
+        Position enemyPosition = mapInfo.getEnemyPosition(hero);
+        restrictedNode.add(enemyPosition);
 
-        Listener onTickTackListener = objects -> {
-            // This is getting the game information from the server.
-            GameInfo gameInfo = GameUtil.getGameInfo(objects);
-            MapInfo mapInfo = gameInfo.getMapInfo();
-
-            tm.call(gameInfo.timestamp, 250L,() -> {
-                // This is getting the current position of the player and the enemy position.
-                n = mapInfo.size.rows;
-                m = mapInfo.size.cols;
-                Position currentPosition = mapInfo.getCurrentPosition(player1);
-                //Position enemyPosition = mapInfo.getEnemyPosition(player1);
-                Player bot = new Player(), botEnemy = new Player();
-                for (Player i : mapInfo.players)
-                    if (i.id.equals(player1.getPlayerID()))
-                        bot = i;
-                    else botEnemy = i;
-                int range = max(bot.power, botEnemy.power);
-
-                /// adding restrict node
-                restrictNode = new ArrayList<>();
-                restrictNode = RestrictedUtils.getRestrictedNode(mapInfo, player1, (mapInfo.bombs.size()) > 0);
-
-                /// making valid_cells
-                valid_cells = new ArrayList<>();
-                visit = new int[n][m];
-                matrix = new int[n][m];
-
-                for (int i = 0; i < n; ++i)
-                    for (int j = 0; j < m; ++j) {
-                        visit[i][j] = 0;
-                        matrix[i][j] = mapInfo.mapMatrix[i][j];
-                    }
-                dfs(currentPosition.getRow(), currentPosition.getCol());
-                //System.out.println(valid_cells.size());
-                /// making path
-                if (valid_cells.size() == 1) {
-                    player1.move("");
-                    return;
-                }
-
-                String path = "";
-                boolean take = false;
-                for (Spoil cell : mapInfo.spoils)
-                    if (cell.spoil_type == 5) {
-                        String tmpPath = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell);
-                        if (tmpPath.equals(""))
-                            continue;
-                        take = true;
-                        if (path.equals("") || tmpPath.length() < path.length())
-                            path = tmpPath;
-                    }
-                if (bot.pill > 0) {
-                    for (Human cell : mapInfo.human)
-                        if (cell.infected && valid_cells.contains(cell.position)) {
-                            take = true;
-                            String tmpPath = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell.position);
-                            if (path.equals("") || tmpPath.length() < path.length())
-                                path = tmpPath;
-                        }
-                }
-
-                for (Human cell : mapInfo.human) {
-                    if (!cell.infected && valid_cells.contains(cell.position)) {
-                        String tmpPath = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell.position);
-                        if (tmpPath.equals(""))
-                            continue;
-                        take = true;
-                        if (path.equals("") || tmpPath.length() < path.length())
-                            path = tmpPath;
-                    }
-                }
-                boolean placeBomb = take;
-                for (Position cell : valid_cells) {
-                    if (placeBomb)
-                        break;
-                    int x = cell.getRow();
-                    int y = cell.getCol();
-                    for (int i = 0; i < 4; ++i) {
-                        if (placeBomb)
-                            break;
-                        int u = x + dx[i];
-                        int v = y + dy[i];
-                        if (0 <= u && u < n && 0 <= v && v < m) {
-                            if (matrix[u][v] == 2) {
-                                for (Position cellx : valid_cells) {
-                                    if (placeBomb)
-                                        break;
-                                    if ((cellx.getRow() != x && cellx.getCol() != y) || (mahatmaDistance(cell, cellx) > range)) {
-                                        String addPath = AStarSearch.aStarSearch(matrix, restrictNode, cell, cellx);
-                                        if (addPath.equals(""))
-                                            continue;
-                                        placeBomb = true;path = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell);
-                                        path = path + "b";
-                                        path += addPath;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                player1.move(path);
-            });
-        };
-
-        // This is the code that connects the player to the server.
-        player1.setOnTickTackListener(onTickTackListener);
-        player1.connectToServer(SERVER_URL);
-    }
-
-    private static void dfs(int x, int y) {
-        visit[x][y] = 1;
-        valid_cells.add(new Position(y, x));
-        for(int i = 0; i < 4; ++i) {
-            int u = x + dx[i];
-            int v = y + dy[i];
-            if(0 <= u && u < n && 0 <= v && v < m && visit[u][v] == 0 && matrix[u][v] == 0 && (!restrictNode.contains(new Position(v, u))))
-                dfs(u, v);
+        if (bombIncluded) {
+            addExplosionNode(restrictedNode, mapInfo.getBombList());
         }
+
+        if (mapInfo.getPlayerByKey(hero.getPlayerID()).pill == 0) {
+            addInfectedNode(restrictedNode, mapInfo);
+        }
+
+        return new ArrayList<>(restrictedNode);
     }
-    private static int mahatmaDistance(Position x, Position y) {
-        return  abs(x.getRow() - y.getRow()) + abs(x.getCol() - y.getCol());
+
+    public static Set<Position> getPermanentRestrictedNode(MapInfo mapInfo) {
+        if (PERMANENT_RESTRICTED_NODE_SET.size() == 0) {
+            PERMANENT_RESTRICTED_NODE_SET.addAll(mapInfo.teleportGate);
+            PERMANENT_RESTRICTED_NODE_SET.addAll(mapInfo.walls);
+            PERMANENT_RESTRICTED_NODE_SET.addAll(mapInfo.quarantinePlace);
+        }
+
+        return PERMANENT_RESTRICTED_NODE_SET;
+    }
+
+    public static void addExplosionNode(Set<Position> restrictedNode, List<Position> explodedNode) {
+        restrictedNode.addAll(explodedNode);
+    }
+    public static void addExplosionNode(List<Position> restrictedNode, List<Position> explodedNode) {
+        restrictedNode.addAll(explodedNode);
+    }
+
+    public static void addInfectedNode(Set<Position> restrictedNode, MapInfo mapInfo) {
+        for (Viruses virus : mapInfo.viruses) {
+            restrictedNode.add(virus.position);
+            restrictedNode.add(virus.position.nextPosition(virus.direction, 1));
+        }
+
+        for (Human human : mapInfo.getDhuman()) {
+            restrictedNode.add(human.position);
+            restrictedNode.add(human.position.nextPosition(human.direction, 1));
+        }
     }
 }
