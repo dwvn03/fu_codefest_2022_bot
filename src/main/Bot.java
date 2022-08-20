4057dd86d4ca4b5e32c48b4bd304469c0bdb5988
Nguyen Tri Phuc <109999741+phucnthe176442@users.noreply.github.com>
2022-08-20 16:11:18 +0700

    Botv0.0.1

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -7,18 +7,20 @@ import jsclub.codefest.sdk.util.GameUtil;
 import jsclub.codefest.sdk.model.Hero;
 
 import main.constant.GameConfig;
+import main.util.RestrictedUtils;
 
 import java.util.*;
 
-import static java.lang.Math.abs;
-
+import static java.lang.Math.*;
 
 public class Bot {
     final static String SERVER_URL = "https://codefest.jsclub.me/";
     public static int[] dx = {-1, 0, 1, 0};
     public static int[] dy = {0, -1, 0, 1};
-    public static int[][] visit;
-    public static ArrayList<Position> targets;
+    public static int[][] visit, matrix;
+    public static List<Position> valid_cells = new ArrayList<>();
+    public static List<Position> restrictNode = new ArrayList<>();
+    public static int n, m;
     public static void main(String[] args) {
         // Creating a new Hero object with name `player1-xxx` and game id
         // `GameConfig.GAME_ID`.
@@ -30,97 +32,95 @@ public class Bot {
             MapInfo mapInfo = gameInfo.getMapInfo();
 
             // This is getting the current position of the player and the enemy position.
+            n = mapInfo.size.rows;
+            m = mapInfo.size.cols;
             Position currentPosition = mapInfo.getCurrentPosition(player1);
-            Position enemyPosition = mapInfo.getEnemyPosition(player1);
-
-            Player bot = new Player();
-            for(Player i : mapInfo.players)
-                if(i.currentPosition == currentPosition)
+            //Position enemyPosition = mapInfo.getEnemyPosition(player1);
+            Player bot = new Player(), botEnemy = new Player();
+            ;
+            for (Player i : mapInfo.players)
+                if (i.id.equals(player1.getPlayerID()))
                     bot = i;
+                else botEnemy = i;
+            int range = max(bot.power, botEnemy.power);
 
             /// adding restrict node
-            List<Position> restrictNode = new ArrayList<>();
-            for(Viruses i : mapInfo.viruses)
-                restrictNode.add(i.position);
-            for(Spoil i : mapInfo.spoils)
-                if(i.spoil_type != 5)
-                    restrictNode.add(new Position(i.getRow(), i.getCol()));
-            restrictNode.addAll(mapInfo.quarantinePlace);
-            restrictNode.addAll(mapInfo.teleportGate);
-            restrictNode.addAll(mapInfo.balk);
-            restrictNode.addAll(mapInfo.getBombList());
+            restrictNode = new ArrayList<>();
+            restrictNode = RestrictedUtils.getRestrictedNode(mapInfo, player1, (mapInfo.bombs.size()) > 0);
 
-            /// making targets list
-            targets = new ArrayList<>();
-            int n = mapInfo.size.rows;
-            int m = mapInfo.size.cols;
+            /// making valid_cells
+            valid_cells = new ArrayList<>();
             visit = new int[n][m];
+            matrix = new int[n][m];
+
             for(int i = 0; i < n; ++i)
                 for(int j = 0; j < m; ++j) {
                     visit[i][j] = 0;
-                    if(mapInfo.mapMatrix[i][j] == 1)
-                        restrictNode.add(new Position(i, j));
+                    matrix[i][j] = mapInfo.mapMatrix[i][j];
                 }
-            boolean containWall = true;
-            dfs(currentPosition.getRow(), currentPosition.getCol(), n, m, restrictNode);
-            for(Position cell : targets) {
-                int x = cell.getRow();
-                int y = cell.getCol();
-                if (0 <= x && x < n && 0 <= y && y < m && mapInfo.mapMatrix[x][y] == 5)
-                    containWall = false;
+            dfs(currentPosition.getRow(), currentPosition.getCol());
+            //System.out.println(valid_cells.size());
+            /// making path
+            if(valid_cells.size() == 1) {
+                player1.move("");
+                return;
             }
 
-
-            /// choosing best path
-            Map<Position, String> ok = AStarSearch.getPathToAllTargets(mapInfo.mapMatrix, restrictNode, currentPosition, targets);
-            String path = new String();
-            if(containWall) {
-                for (Position cell : targets) {
-                    if (ok.containsKey(cell)) {
-                        if(cell == currentPosition)
-                            continue;
-                        String tmpPath = ok.get(cell);
+            String path = "";
+            boolean takePill = false;
+            for(Spoil cell : mapInfo.spoils)
+                if(cell.spoil_type == 5) {
+                    String tmpPath = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell);
+                    if(tmpPath.equals(""))
+                        continue;
+                    takePill = true;
+                    if(path.equals("") || tmpPath.length() < path.length())
+                        path = tmpPath;
+                }
+            if(!takePill) {
+                boolean takeCured = false;
+                if(bot.pill > 0) {
+                    for(Human cell : mapInfo.human)
+                        if(cell.infected && valid_cells.contains(cell.position)) {
+                            takeCured = true;
+                            String tmpPath = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell.position);
+                            if(path.equals("") || tmpPath.length() < path.length())
+                                path = tmpPath;
+                        }
+                }
+                if(!takeCured) {
+                    boolean placeBomb = false;
+                    for(Position cell : valid_cells) {
+                        if(placeBomb)
+                            break;
                         int x = cell.getRow();
                         int y = cell.getCol();
-                        for(Position cell1 : targets)
-                            if(cell1 != cell) {
-                                boolean hudge = true;
-                                int u = cell1.getRow();
-                                int v = cell1.getCol();
-                                if (u == x && (y - bot.power + 1 <= v && v <= y))
-                                    hudge = false;
-                                else if(u == x && (y <= v && v <= y + bot.power - 1))
-                                    hudge = false;
-                                else if(x <= u && u <= x + bot.power - 1 && v == y)
-                                    hudge = false;
-                                else if(x - bot.power + 1 <= u && u <= x && v == y)
-                                    hudge = false;
-                                if(hudge) {
-                                    String path2 = AStarSearch.aStarSearch(mapInfo.mapMatrix, restrictNode, cell, cell1);
-                                    if(path.isEmpty() || path2.length()+tmpPath.length()+1 < path.length()) {
-                                        path = tmpPath + "b" + path2;
+                        for(int i = 0; i < 4; ++i) {
+                            if(placeBomb)
+                                break;
+                            int u = x + dx[i];
+                            int v = y + dy[i];
+                            if(0 <= u && u < n && 0 <= v && v < m) {
+                                if(matrix[u][v] == 2) {
+                                    for(Position cellx : valid_cells) {
+                                        if(placeBomb)
+                                            break;
+                                        if((cellx.getRow() != x && cellx.getCol() != y) || (mahatmaDistance(cell, cellx) > range)) {
+                                            String addPath = AStarSearch.aStarSearch(matrix, restrictNode, cell, cellx);
+                                            if(addPath.equals(""))
+                                                continue;
+                                            placeBomb = true;
+                                            path = AStarSearch.aStarSearch(matrix, restrictNode, currentPosition, cell);
+                                            path = path + "b";
+                                            path += addPath;
+                                        }
                                     }
                                 }
                             }
-                    }
-                }
-            } else {
-                for (Position cell : targets) {
-                    if(ok.containsKey(cell) && mapInfo.mapMatrix[cell.getRow()][cell.getCol()] == 5) {
-                        String tmpPath = ok.get(cell);
-                        if(path.isEmpty() || tmpPath.length() < path.length())
-                            path = tmpPath;
-                    }
-                }
-                if(path.isEmpty()) {
-                    for(Position cell : targets)
-                        if(ok.containsKey(cell)){
-                            if(path.isEmpty() || ok.get(cell).length() < path.length())
-                                path = ok.get(cell);
                         }
+                    }
                 }
             }
-
             player1.move(path);
         };
 
@@ -129,22 +129,17 @@ public class Bot {
         player1.connectToServer(SERVER_URL);
     }
 
-    private static Boolean reachable(Position n, List<Position> restrictNode) {
-        return !restrictNode.contains(n);
-    }
-
-    private static void dfs(int x, int y, int n, int m, List<Position> restrictNode){
+    private static void dfs(int x, int y) {
         visit[x][y] = 1;
-        targets.add(new Position(x, y));
+        valid_cells.add(new Position(y, x));
         for(int i = 0; i < 4; ++i) {
             int u = x + dx[i];
             int v = y + dy[i];
-            if(0 <= u && u < n && 0 <= v && v < m){
-                if(visit[u][v] == 0) {
-                    if(reachable(new Position(u, v), restrictNode))
-                        dfs(u, v, n, m, restrictNode);
-                }
-            }
+            if(0 <= u && u < n && 0 <= v && v < m && visit[u][v] == 0 && matrix[u][v] == 0 && (!restrictNode.contains(new Position(v, u))))
+                dfs(u, v);
         }
+    }
+    private static int mahatmaDistance(Position x, Position y) {
+        return  abs(x.getRow() - y.getRow()) + abs(x.getCol() - y.getCol());
     }
 }
